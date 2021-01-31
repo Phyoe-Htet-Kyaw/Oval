60c7ef75806be3cb1a5533b8ba422e862a3ef50a
Phyoe Htet Kyaw <pphyoe126@gmail.com>
2021-01-31 10:18:12 +0630

    setting up develop project

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\user\documents\myproject\oval\develop\venv\scripts\python.exe
+#!c:\users\user\documents\myproject\oval\venv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
